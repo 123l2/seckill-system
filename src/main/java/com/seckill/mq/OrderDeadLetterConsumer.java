0226de0885cf34c4359d5bf815da437693d3fb52
123l2 <2684164109@qq.com>
2026-07-25 15:31:11 +0800

    Update src/main/java/com/seckill/mq/OrderDeadLetterConsumer.java

--- src/main/java/com/seckill/mq/OrderDeadLetterConsumer.java
+++ src/main/java/com/seckill/mq/OrderDeadLetterConsumer.java
@@ -2,6 +2,7 @@
 
 import com.seckill.config.RabbitMQConfig;
 import com.seckill.entity.Order;
+import com.seckill.service.OrdersService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -12,9 +13,15 @@ public class OrderDeadLetterConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(OrderDeadLetterConsumer.class);
 
+    private final OrdersService ordersService;
+
+    public OrderDeadLetterConsumer(OrdersService ordersService) {
+        this.ordersService = ordersService;
+    }
+
     @RabbitListener(queues = RabbitMQConfig.ORDER_DEAD_LETTER_QUEUE)
     public void handleExpiredOrder(Order order) {
         log.info("Order {} expired, auto-cancelling", order.getOrderNo());
-        // update order status to cancelled in DB
+        ordersService.cancelOrder(order.getOrderNo());
     }
 }
