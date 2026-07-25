75ddd27fc75cc97d0ae714361193d4703e78d94f
123l2 <2684164109@qq.com>
2026-07-25 14:00:37 +0800

    Add src/main/java/com/seckill/entity/Stock.java

---
+++ src/main/java/com/seckill/entity/Stock.java
@@ -0,0 +1,23 @@
+﻿package com.seckill.entity;
+
+public class Stock {
+    private Long id;
+    private Long productId;
+    private String productName;
+    private Integer totalStock;
+    private Integer remainingStock;
+    private Integer version;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public Long getProductId() { return productId; }
+    public void setProductId(Long productId) { this.productId = productId; }
+    public String getProductName() { return productName; }
+    public void setProductName(String productName) { this.productName = productName; }
+    public Integer getTotalStock() { return totalStock; }
+    public void setTotalStock(Integer totalStock) { this.totalStock = totalStock; }
+    public Integer getRemainingStock() { return remainingStock; }
+    public void setRemainingStock(Integer remainingStock) { this.remainingStock = remainingStock; }
+    public Integer getVersion() { return version; }
+    public void setVersion(Integer version) { this.version = version; }
+}
