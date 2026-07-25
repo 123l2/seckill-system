dbf5b8cf1b340bd87cc8a44724a2bb919d0a6fda
123l2 <2684164109@qq.com>
2026-07-25 14:00:41 +0800

    Add src/main/java/com/seckill/mq/OrderMessageSender.java

---
+++ src/main/java/com/seckill/mq/OrderMessageSender.java
@@ -0,0 +1,27 @@
+﻿package com.seckill.mq;
+
+import com.seckill.config.RabbitMQConfig;
+import com.seckill.entity.Order;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderMessageSender {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public OrderMessageSender(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendOrderMessage(Order order, long delayMs) {
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.ORDER_DELAY_EXCHANGE,
+                RabbitMQConfig.ROUTING_KEY_DELAY,
+                order,
+                msg -> {
+                    msg.getMessageProperties().setDelayLong(delayMs);
+                    return msg;
+                });
+    }
+}
