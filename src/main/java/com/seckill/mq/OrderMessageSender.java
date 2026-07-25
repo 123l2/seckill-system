9fbd9b5ea3c49ccc1813070b8b74546011a56fb2
123l2 <2684164109@qq.com>
2026-07-25 15:31:09 +0800

    Update src/main/java/com/seckill/mq/OrderMessageSender.java

--- src/main/java/com/seckill/mq/OrderMessageSender.java
+++ src/main/java/com/seckill/mq/OrderMessageSender.java
@@ -2,6 +2,7 @@
 
 import com.seckill.config.RabbitMQConfig;
 import com.seckill.entity.Order;
+import com.seckill.service.OrdersService;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
@@ -9,12 +10,18 @@ import org.springframework.stereotype.Component;
 public class OrderMessageSender {
 
     private final RabbitTemplate rabbitTemplate;
+    private final OrdersService ordersService;
 
-    public OrderMessageSender(RabbitTemplate rabbitTemplate) {
+    public OrderMessageSender(RabbitTemplate rabbitTemplate, OrdersService ordersService) {
         this.rabbitTemplate = rabbitTemplate;
+        this.ordersService = ordersService;
     }
 
     public void sendOrderMessage(Order order, long delayMs) {
+        // persist order first
+        ordersService.saveOrder(order);
+
+        // send to delay queue
         rabbitTemplate.convertAndSend(
                 RabbitMQConfig.ORDER_DELAY_EXCHANGE,
                 RabbitMQConfig.ROUTING_KEY_DELAY,
