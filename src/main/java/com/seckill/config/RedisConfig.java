69a445a2e8e3f503274a46e1bf7bb558339b3c21
123l2 <2684164109@qq.com>
2026-07-25 14:00:28 +0800

    Add src/main/java/com/seckill/config/RedisConfig.java

---
+++ src/main/java/com/seckill/config/RedisConfig.java
@@ -0,0 +1,21 @@
+﻿package com.seckill.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
+
+import java.util.List;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisScript<Long> seckillScript() {
+        DefaultRedisScript<Long> script = new DefaultRedisScript<>();
+        script.setLocation(new ClassPathResource("seckill.lua"));
+        script.setResultType(Long.class);
+        return script;
+    }
+}
